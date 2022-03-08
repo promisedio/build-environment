742e96abbaa2c667d76fca9d9c66ce65f5f38786
aachurin <aachurin@gmail.com>
2022-03-09 00:20:41 +0300

    clinic now creates can write documentation to README.md

--- promisedio_build_environment/clinic.py
+++ promisedio_build_environment/clinic.py
@@ -8,70 +8,138 @@ from . pyclinic import *
 from . pyclinic import main as clinic_main
 
 
-_readme_content = {}
+readme_contents = {}
+
+format_to_signature = {
+    "s": "str",                                 # [const char *]
+    "s*": "Union[str, bytes, bytearray]",       # [Py_buffer]
+    "s#": "Union[str, bytes]",                  # [const char *, Py_ssize_t]
+    "z": "Optional[str]",                       # [const char *]
+    "z*": "Optional[Union[str, bytes, bytearray]]", # [Py_buffer]
+    "z#": "Optional[Union[str, bytes]]",        # [const char *, Py_ssize_t]
+    "y": "bytes",                               # [const char *]
+    "y*": "Union[bytes, bytearray]",            # [Py_buffer]
+    "y#": "bytes",                              # [const char *, Py_ssize_t]
+    "S": "bytes",                               # [PyBytesObject *]
+    "Y": "bytearray",                           # [PyByteArrayObject *]
+    "u": "str",                                 # [const Py_UNICODE *]
+    "u#": "str",                                # [const Py_UNICODE *, Py_ssize_t]
+    "Z": "Optional[str]",                       # [const Py_UNICODE *]
+    "Z#": "Optional[str]",                      # [const Py_UNICODE *, Py_ssize_t]
+    "U": "str",                                 # [PyObject *]
+    "w*": "bytearray",                          # [Py_buffer]
+    "es": "str",                                # [const char *encoding, char **buffer]
+    "et": "Union[str, bytes, bytearray]",       # [const char *encoding, char **buffer]
+    "es#": "str",                               # [const char *encoding, char **buffer, Py_ssize_t *buffer_length]
+    "et#": "Union[str, bytes, bytearray]",      # [const char *encoding, char **buffer, Py_ssize_t *buffer_length]
+    "b": "int",                                 # [unsigned char]
+    "B": "int",                                 # [unsigned char]
+    "h": "int",                                 # [short int]
+    "H": "int",                                 # [unsigned short int]
+    "i": "int",                                 # [int]
+    "I": "int",                                 # [unsigned int]
+    "l": "int",                                 # [long int]
+    "k": "int",                                 # [unsigned long]
+    "L": "int",                                 # [long long]
+    "K": "int",                                 # [unsigned long long]
+    "n": "int",                                 # [Py_ssize_t]
+    "c": "Union[bytes, bytearray]",             # [char]
+    "C": "str",                                 # [int]
+    "f": "float",                               # [float]
+    "d": "float",                               # [double]
+    "D": "complex",                             # [Py_complex]
+    "O": "object",
+    "O!": "object",
+    "O&": "object",
+    "p": "bool",                                # [bool predicate]
+}
 
 
-def get_readme():
-    if _readme_content:
-        return _readme_content
-    with open("README.md", "rt") as f:
-        data = f.read()
-    content = _readme_content
-    module = func = None
-    lines = data.splitlines()
-    n = 0
-    while lines:
-        line = lines.pop(0)
-        n += 1
-        if line.startswith("### "):
-            module = line.split()[1]
-            func = None
-            continue
-        if module:
-            if line.startswith("```python"):
-                line = lines.pop(0)
-                n += 1
-                func = line.split("(")[0].strip()
-                if func and re.match('^[a-zA-Z_]+(?:\.[a-zA-Z_]+)?$', func):
-                    content.setdefault(module, {})[func] = ""
-                else:
-                    func = None
-                while lines.pop(0).strip() != "```":
-                    n += 1
-                continue
-            if func:
-                content[module][func] += line + "\n"
-    return content
+def get_parameter_annotation(param):
+    arg = param.name
+    arg_type = getattr(param.converter, "typed", None)
+    if not arg_type and param.converter.format_unit in format_to_signature:
+        arg_type = format_to_signature[param.converter.format_unit]
+    if not arg_type:
+        arg_type = "Any"
+    arg += ": " + arg_type
+    if param.default is not unspecified:
+        arg += " = " + repr(param.default)
+    return arg
 
 
-def get_readme_description(func):
-    result = func.docstring.rstrip("\n") + "\n\n"
-    module = func.module.name
-    name = f"{func.cls.name}.{func.name}" if func.cls else func.name
-    if name.endswith(".__new__"):
-        name = name[:-8]
-    lines = get_readme().get(module, {}).get(name)
-    if not lines:
-        print("No docstring for %s.%s" % (module, name))
-    else:
-        result += lines
-    return result
+def get_return_annotation(func):
+    converter = func.return_converter
+    annotation = getattr(converter, "typed", None)
+    return annotation or "Any"
 
 
 docstring_for_c_string = CLanguage.docstring_for_c_string
 
 
-class DocstringHolder:
-    pass
-
-
 def docstring_for_c_string_from_readme(self, f):
-    df = DocstringHolder()
-    df.docstring = get_readme_description(f)
-    return docstring_for_c_string(self, df)
+    result = docstring_for_c_string(self, f)
+    module = readme_contents.setdefault(f.module.name, {"classes": {}, "functions": {}})
+    if f.cls:
+        module["classes"].setdefault(f.cls.name, {})[f.name] = f
+    else:
+        module["functions"][f.name] = f
+    return result
 
 
 CLanguage.docstring_for_c_string = docstring_for_c_string_from_readme
+
+
+def generate_readme():
+    output = []
+
+    def replacer(m):
+        name = m.group(1)
+        if name in functions or name in classes:
+            return f"[{name}](#{name.lower()})"
+        return f"`{name}`"
+
+    def generate_function(name, f):
+        args = [get_parameter_annotation(p) for p in list(f.parameters.values())[1:]]
+        returns = get_return_annotation(f)
+        output.append(f"#### {name}")
+        # output.append(f"##### Signature")
+        output.append("```python")
+        output.append(f"{name}({', '.join(args)}) -> {returns}")
+        output.append("```")
+        _, doc = f.docstring.split("--", 1)
+        doc = doc.strip()
+        doc = re.sub(r"`([^`]*)`", replacer, doc)
+        # output.append(f"##### Description")
+        output.append(doc)
+        output.append("")
+
+    for module in sorted(readme_contents):
+        classes = readme_contents[module]["classes"]
+        functions = readme_contents[module]["functions"]
+        output.append(f"# {module} module")
+        for function in sorted(functions):
+            generate_function(function, functions[function])
+        for cls in sorted(classes):
+            output.append(f"### {cls}")
+            functions = classes[cls]
+            for function in sorted(functions):
+                generate_function(f"{cls}.{function}", functions[function])
+        output.append("")
+
+        template = open("README.md").read()
+        pattern = rf"<!---\s*template:\[{module}\]\s*-->[\s\S]*<!---\s*end:\[{module}\]\s*-->"
+
+        def repl(m):
+            return (
+                f"<!--- template:[{module}] -->\n" +
+                "\n".join(output) +
+                f"\n<!--- end:[{module}] -->"
+            )
+
+        doc = re.sub(pattern, repl, template, 1)
+        if doc != template:
+            open("README.md", "wt").write(doc)
 
 
 def rebuild_func(fn, consts):
@@ -113,6 +181,7 @@ class Path_converter(CConverter):
     type = "PyObject *"
     converter = "PyUnicode_FSConverter"
     c_default = "NULL"
+    typed = "Union[Path, str, bytes]"
 
     def cleanup(self):
         return f"Py_XDECREF({self.name});"
@@ -122,10 +191,12 @@ class cstring_converter(CConverter):
     type = "const char *"
     converter = "cstring_converter"
     c_default = "NULL"
+    typed = "Union[str, bytes]"
 
     def converter_init(self, *, accept=None):
         if accept == {NoneType}:
             self.converter = "cstring_optional_converter"
+            self.typed = "Optional[Union[str, bytes]]"
         elif accept is not None:
             fail("cstring_converter: illegal 'accept' argument " + repr(accept))
 
@@ -133,36 +204,67 @@ class cstring_converter(CConverter):
 class ssize_t_converter(CConverter):
     type = "Py_ssize_t"
     converter = "ssize_t_converter"
+    typed = "int"
 
 
 class fd_converter(CConverter):
     type = "int"
     converter = "fd_converter"
+    typed = "int"
 
 
 class off_t_converter(CConverter):
     type = "Py_off_t"
     converter = "off_t_converter"
+    typed = "int"
 
 
 class inet_addr_converter(CConverter):
     type = "sockaddr_any"
     converter = "inet_addr_converter"
     impl_by_reference = True
+    typed = "Tuple[str, int]"
 
 
 class uid_t_converter(CConverter):
     type = "uid_t"
     converter = "uid_converter"
+    typed = "int"
 
 
 class gid_t_converter(CConverter):
     type = "gid_t"
     converter = "gid_converter"
+    typed = "int"
+
+
+class object_converter(object_converter):
+    typed = "object"
+
+    def converter_init(self, *, typed=None, **kwargs):
+        self.typed = typed
+        super().converter_init(**kwargs)
+
+
+class object_return_converter(CReturnConverter):
+    typed = "object"
+
+    def return_converter_init(self, *, typed=None, **kwargs):
+        self.typed = typed
+        super().return_converter_init(**kwargs)
+
+
+class None_return_converter(CReturnConverter):
+    typed = "None"
+
+
+class Any_return_converter(CReturnConverter):
+    typed = "Any"
 
 
 def main():
     clinic_main(sys.argv[1:])
+    generate_readme()
 
 
 if __name__ == "__main__":
